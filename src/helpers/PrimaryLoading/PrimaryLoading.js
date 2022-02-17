c4c727d0d2b5f5a0f0df2a8dd4cad5f9ebb2f72a
Zihad Malik <zihadm654@gmail.com>
2022-02-17 19:57:08 +0600

    loader spinner added

--- src/helpers/PrimaryLoading/PrimaryLoading.js
+++ src/helpers/PrimaryLoading/PrimaryLoading.js
@@ -1,13 +1,12 @@
 import style from "./PrimaryLoading.module.css";
 import Logo from "../../assets/images/logo.png";
+import { CircularProgress } from "@material-ui/core";
 const PrimaryLoading = () => {
   return (
     <>
       <div className={style.mainLoadingContainer}>
         <img src={Logo} alt="" />
-        <div className="spinner-border text-dark" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
+        <CircularProgress color="inherit" />
       </div>
     </>
   );
